981adedcef0c6d839d9065c77c5faf61a30529b1
Julierme Heinstein <julierme.jadon@outlook.com>
2018-02-20 15:24:43 -0300

    Upando Arquivos
    Contém pasta para utilização no Visual Studio

---
+++ ListaFuncionarioEstatica.h
@@ -0,0 +1,29 @@
+/* TAD: Lista */
+
+/* Tipo exportado */
+typedef struct lista ListaFuncionarioEstatica;
+
+/***********************************************************/
+/******************* Funções exportadas *******************/
+
+/* Função cria Lista- Aloca e retorna uma Lista de Funcionários */
+ListaFuncionarioEstatica* lista_cria(int);
+
+/* Função insere no inicio - Insere um Novo Funcionário no inicio da Lista */
+ListaFuncionarioEstatica* lista_insere_inicio(ListaFuncionarioEstatica* list, Funcionario* func);
+
+/* Função busca- busca um Funcionario na lista */
+Funcionario* busca(ListaFuncionarioEstatica* lst, int v);
+
+/* Função Lista Vazia- Testa de a Lista está vazia  */
+int lista_vazia(ListaFuncionarioEstatica* list);
+
+/* Função Remove - Remove um Funcionário da Lista */
+ListaFuncionarioEstatica* lista_remove(ListaFuncionarioEstatica* list, int val);
+
+/* Função Imprime - Imprime uma Lista de Funcionários*/
+void lista_imprime(ListaFuncionario* list);
+
+/*Libera o espaço de memória da lista. Retorna NULL para manter uma referêcia da lista
+no programa cliente;*/
+ListaFuncionario* lista_libera(ListaFuncionario* list);
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Exercicio-Estrutura
 Referente a atividade do dia 20/02
+
+#Grupo
+Isleimar, José, Julierme, Victor, Leanderson
