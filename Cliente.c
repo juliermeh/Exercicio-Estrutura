790b78ec0b49955693de986f8ef29b6af67e9381
Julierme Heinstein <julierme.jadon@outlook.com>
2018-02-22 14:55:07 -0300

    Incrementando Cliente
    Versão parcialmente funcional

--- Cliente.c
+++ Cliente.c
@@ -34,100 +34,189 @@ int main(int argc, char *argv[]) {
 
 	if (opcao == 0) {
 		ListaFuncionario* list = lista_cria();
-	}
-	else {
+
+		do {
+
+			int valor = 0;
+			imprime_menu(); // mostra o menu sempre depois de uma ação
+			scanf("%d", &valor);
+
+			switch (valor) {
+
+			case 1:   //Adiciona um Funcionario
+			{
+				Funcionario* f = cria_Funcionario();
+				if (f != NULL) {
+
+					imprime_Func(f);
+					system("pause"); //espera teclar
+					system("cls"); //limpa a tela
+					list = lista_insere_inicio(list, f);
+					printf("Funcionario adicionado com Sucesso!!\n");
+					system("pause"); //espera teclar
+					system("cls"); //limpa a tela
+
+				}
+				else printf("Memoria insuficiente!!\n");
+			}
+			break;
+
+			case 2:    //Remove Funcionário
+			{
+				int mat = 0;
+				printf("Digite a matricula do Funcionario\n");
+				scanf("%d", &mat);
+				list = lista_remove(list, mat);
+				system("pause"); //espera teclar
+				system("cls"); //limpa a tela
+			}
+			break;
+
+			case 3:   //Busca Funcionario
+			{
+				int mat = 0;
+				printf("Digite a matricula do Funcionario\n");
+				scanf("%d", &mat);
+				Funcionario* f_aux = busca(list, mat);
+				if (f_aux != NULL) {
+
+					imprime_Func(f_aux);
+					system("pause");
+					system("cls");
+
+				}
+				else {
+					printf("Nao foi possivel retornar Funcionario\n");
+					system("pause");
+					system("cls");
+
+				}
+				break;
+			}
+			case 4:    //Imprime a Lista de Funcionarios
+			{
+				printf("\n=========== E M P R E S A ================\n");
+				printf("==========================================\n\n");
+				lista_imprime(list);
+				printf("==========================================\n\n");
+				system("pause"); //espera teclar
+				system("cls"); //limpa a tela
+			}
+			break;
+			case 5:  //Deleta a Lista
+			{
+				/*List receberá NULL após liberação do espaço de memória
+				para o programa manter uma referencia da Lista*/
+				list = lista_libera(list);
+			}
+
+			break;
+
+			default:
+			{
+				printf("Valor invalido!\n\n");
+				system("pause");
+				system("cls");
+			}
+			}
+
+		} while (1); //Loop infinito
+	} else {
 		int tamanho = 10;
 		printf("Para o tamanho padrão (10) aperte enter");
 		printf("Digite o tamanho da lista: ");
 		scanf(tamanho);
-		ListaFuncionarioEstatica * list = lista_cria(tamanho);
-	}
+		ListaFuncionarioEstatica* list = listaestatica_cria(tamanho);
 
-	do{
-		
-		int valor=0;
-		imprime_menu(); // mostra o menu sempre depois de uma ação
-		scanf ("%d",&valor);
-			
-		switch ( valor ) {
-			
-		     case 1 :   //Adiciona um Funcionario
-		       {
-		       	Funcionario* f = cria_Funcionario();
-			       	if(f!=NULL){
-					   
-					    imprime_Func(f);
-				       	system("pause"); //espera teclar
-				       	system("cls"); //limpa a tela
-				       	list = lista_insere_inicio(list,f);
-			       	    printf("Funcionario adicionado com Sucesso!!\n");
-			       	    system("pause"); //espera teclar
-				       	system("cls"); //limpa a tela
-				       	
-			       }else printf("Memoria insuficiente!!\n");
-			   }
-		     break;
-		 
-		     case 2 :    //Remove Funcionário
-		       {
-		       	int mat=0;
-		        printf("Digite a matricula do Funcionario\n");
-				scanf ("%d",&mat);
+		do {
+
+			int valor = 0;
+			imprime_menu(); // mostra o menu sempre depois de uma ação
+			scanf("%d", &valor);
+
+			switch (valor) {
+
+			case 1:   //Adiciona um Funcionario
+			{
+				Funcionario* f = cria_Funcionario();
+				if (f != NULL) {
+
+					imprime_Func(f);
+					system("pause"); //espera teclar
+					system("cls"); //limpa a tela
+					list = lista_insere_inicio(list, f);
+					printf("Funcionario adicionado com Sucesso!!\n");
+					system("pause"); //espera teclar
+					system("cls"); //limpa a tela
+
+				}
+				else printf("Memoria insuficiente!!\n");
+			}
+			break;
+
+			case 2:    //Remove Funcionário
+			{
+				int mat = 0;
+				printf("Digite a matricula do Funcionario\n");
+				scanf("%d", &mat);
 				list = lista_remove(list, mat);
 				system("pause"); //espera teclar
 				system("cls"); //limpa a tela
-			   }
-		     break;
-		 
-		     case 3 :   //Busca Funcionario
-		        {
-		        int mat=0;
-		        printf("Digite a matricula do Funcionario\n");
-				scanf ("%d",&mat);
-				Funcionario* f_aux = busca(list,mat);
-				if(f_aux!=NULL){
-				
+			}
+			break;
+
+			case 3:   //Busca Funcionario
+			{
+				int mat = 0;
+				printf("Digite a matricula do Funcionario\n");
+				scanf("%d", &mat);
+				Funcionario* f_aux = busca(list, mat);
+				if (f_aux != NULL) {
+
 					imprime_Func(f_aux);
 					system("pause");
 					system("cls");
-					 
-		    	}
+
+				}
 				else {
-						printf("Nao foi possivel retornar Funcionario\n");
-						system("pause");
-						system("cls");	
-						 	
-					}
-		         break;
-	     	   }
-			 case 4 :    //Imprime a Lista de Funcionarios
-		    {
-		        printf("\n=========== E M P R E S A ================\n");
-                printf("==========================================\n\n");
-		      	lista_imprime(list);
-		      	printf("==========================================\n\n");
-		      	system("pause"); //espera teclar
-			    system("cls"); //limpa a tela
-			  }
-		     break;
-		     case 5 :  //Deleta a Lista
-		      {
-		      	/*List receberá NULL após liberação do espaço de memória
-				  para o programa manter uma referencia da Lista*/
-		      	list = lista_libera(list); 
-			  }
+					printf("Nao foi possivel retornar Funcionario\n");
+					system("pause");
+					system("cls");
+
+				}
+				break;
+			}
+			case 4:    //Imprime a Lista de Funcionarios
+			{
+				printf("\n=========== E M P R E S A ================\n");
+				printf("==========================================\n\n");
+				listaestatica_imprime(list);
+				printf("==========================================\n\n");
+				system("pause"); //espera teclar
+				system("cls"); //limpa a tela
+			}
+			break;
+			case 5:  //Deleta a Lista
+			{
+				/*List receberá NULL após liberação do espaço de memória
+				para o programa manter uma referencia da Lista*/
+				list = listaestatica_libera(list);
+			}
+
+			break;
+
+			default:
+			{
+				printf("Valor invalido!\n\n");
+				system("pause");
+				system("cls");
+			}
+			}
+
+		} while (1); //Loop infinito
+	}
+
 	
-		     break;	 
-		     	 
-		     default :
-		     	{
-				   printf ("Valor invalido!\n\n");
-				   system("pause");
-				   system("cls");
-		     	}
-        }
-		
-	}while(1); //Loop infinito
        
  }
  
--- ListaFuncionarioEstatica.h
+++ ListaFuncionarioEstatica.h
@@ -7,23 +7,23 @@ typedef struct lista ListaFuncionarioEstatica;
 /******************* Funções exportadas *******************/
 
 /* Função cria Lista- Aloca e retorna uma Lista de Funcionários */
-ListaFuncionarioEstatica* lista_cria(int);
+ListaFuncionarioEstatica* listaestatica_cria(int);
 
 /* Função insere no inicio - Insere um Novo Funcionário no inicio da Lista */
-ListaFuncionarioEstatica* lista_insere_inicio(ListaFuncionarioEstatica* list, Funcionario* func);
+ListaFuncionarioEstatica* listaestatica_insere_inicio(ListaFuncionarioEstatica* list, Funcionario* func);
 
 /* Função busca- busca um Funcionario na lista */
-Funcionario* busca(ListaFuncionarioEstatica* lst, int v);
+Funcionario* buscaestatica(ListaFuncionarioEstatica* lst, int v);
 
 /* Função Lista Vazia- Testa de a Lista está vazia  */
-int lista_vazia(ListaFuncionarioEstatica* list);
+int listaestatica_vazia(ListaFuncionarioEstatica* list);
 
 /* Função Remove - Remove um Funcionário da Lista */
-ListaFuncionarioEstatica* lista_remove(ListaFuncionarioEstatica* list, int val);
+ListaFuncionarioEstatica* listaestatica_remove(ListaFuncionarioEstatica* list, int val);
 
 /* Função Imprime - Imprime uma Lista de Funcionários*/
-void lista_imprime(ListaFuncionario* list);
+void listaestatica_imprime(ListaFuncionarioEstatica* list);
 
 /*Libera o espaço de memória da lista. Retorna NULL para manter uma referêcia da lista
 no programa cliente;*/
-ListaFuncionario* lista_libera(ListaFuncionario* list);
+ListaFuncionarioEstatica* listaestatica_libera(ListaFuncionarioEstatica* list);
